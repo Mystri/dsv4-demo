76a87189e00dad818581d5667b26ebbc640a62cf
Mystri <boyouhan@gmail.com>
2026-05-20 13:04:43 +0800

    Initial commit: first graph break

---
+++ graph_break_lab.py
@@ -0,0 +1,14 @@
+import torch
+import os
+
+os.environ["TORCH_LOGS"] = "graph_breaks"
+
+@torch.compile
+def fn(x):
+    if x.sum() > 0:      # 隐式 .item()
+        return x * 2
+    return x
+
+break_if(torch.randn(3))
+
+torch._dynamo.explain(fn)(x)
